6961ab8853d154dddcff9b5e2266a974c4716548
joe.chapman <joe.chapman@dwp.gsi.gov.uk>
2018-12-20 12:46:33 +0000

    a

--- app/routes/routes.js
+++ app/routes/routes.js
@@ -1,3 +1,14 @@
 module.exports = (app) => {
     app.get('/', (req, res) => res.render('index'));
+
+    app.post('/', [
+        (req, res, next) => {
+            console.log('here');
+            return res.redirect('/requester/form');
+        }
+    ]);
+
+    app.get('/requester/form', (req, res) => res.render('requester/form'));
+
+
 };
