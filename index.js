db639c071fa88925570593de4bef90bf740e3130
joe.chapman <joe.chapman@dwp.gsi.gov.uk>
2018-12-20 11:00:25 +0000

    rework https

--- index.js
+++ index.js
@@ -10,13 +10,13 @@ const app = express();
 
 bootstrap(app);
 
-if(process.env === 'local') {
+if(process.env.NODE_ENV === 'local') {
     const options = {
         key: fs.readFileSync(process.env.KEY_FILE_PATH),
         cert: fs.readFileSync(process.env.CERT_FILE_PATH)
     };
     const server = https.createServer(options, app);
-    server.listen(PORT, () => console.log(`Recruitment client listening on port: ${PORT}`));
+    server.listen(PORT, () => console.log(`Secure Recruitment client listening on port: ${PORT}`));
 } else {
     app.listen(PORT, () => console.log(`Recruitment client listening on port: ${PORT}`));
 }
